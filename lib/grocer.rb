c59deabf5425afa93309f3d94ffd07171b9f0a42
Christy Tropila <ctropilalakebeverage@gmail.com>
2020-08-01 15:57:07 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -2,6 +2,9 @@ require 'pry'
 
 def find_item_by_name_in_collection(name, collection)
   collection.each do |grocery_hash|
+    if grocery_hash[:item]==name
+      puts "hello"
+    end
     binding.pry
 
   end
